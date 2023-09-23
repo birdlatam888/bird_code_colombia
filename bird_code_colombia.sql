7a60b9b2dce6b65a658d2d236fda69c71807cedd
geovanny999 <gamard199@gmail.com>
2023-09-22 21:27:28 -0500

    Creation of a table of hummingbird species in Valle del Cauca according to the Global Biodiversity Information Facility, GBIF

--- bird_code_colombia.sql
+++ bird_code_colombia.sql
@@ -1,2 +1,10 @@
 CREATE EXTENSION postgis;
 
+--- Creation of a table of hummingbird species and number of records in 
+--- Valle del Cauca according to the "Global Biodiversity Information Facility, GBIF"
+
+--- Total number of hummingbird records in Valle del Cauca as of September 2023 
+--- according to GBIF: 115,389
+
+SELECT species, COUNT(DISTINCT id) AS GBIF_records FROM hummingbird_valle
+GROUP BY species ORDER BY GBIF_records DESC;
